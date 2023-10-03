960852224ca39fe9d25524c3b0ff3ac90ecd9851
tekostyshyn <tekostyshyn@gmail.com>
2023-10-03 16:02:45 +0300

    Generic tasks finished

--- src/generic/4.ts
+++ src/generic/4.ts
@@ -2,16 +2,19 @@
   Використовуйте generics та інтерфейси, щоб виправити помилку в наступних класах:
 */
 
-class Component {
-  constructor (public props:T) {
+interface IProps {
+  title: string
+}
 
+class Component<T> {
+  constructor(public props: T) {
   }
 }
 
-class Page extends Component {
-  pageInfo () {
+class Page extends Component<IProps> {
+  pageInfo() {
     console.log(this.props.title);
   }
 }
 
-export {};
+export { };
