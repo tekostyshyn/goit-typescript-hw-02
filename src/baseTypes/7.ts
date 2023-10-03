2006d501ed9daf015c256784ab21c38d546058d9
tekostyshyn <tekostyshyn@gmail.com>
2023-10-03 15:15:15 +0300

    baseTypes tasks completed

--- src/baseTypes/7.ts
+++ src/baseTypes/7.ts
@@ -2,3 +2,32 @@
   Створіть функцію (isWeekend), яка приймає день тижня (з вашого enum)
   і повертає boolean значення, що вказує, чи це день робочий чи вихідний.
 */
+
+enum Day {
+  MON,
+  TUE,
+  WED,
+  THU,
+  FRI,
+  SAT,
+  SUN
+}
+
+function isWeekend(day: Day): boolean {
+  switch (day) {
+    case Day.MON:
+      return false;
+    case Day.TUE:
+      return false;
+    case Day.WED:
+      return false;
+    case Day.THU:
+      return false;
+    case Day.FRI:
+      return false;
+    case Day.SAT:
+      return true;
+    case Day.SUN:
+      return true;
+  }
+} 
--- src/baseTypes/9.ts
+++ src/baseTypes/9.ts
@@ -2,7 +2,19 @@
   Створіть новий тип даних, який підходить для цих двох об'єктів.
 */
 
-const page1 = {
+type Base = {
+  title: string,
+  likes: number,
+  accounts: string[],
+  status: string,
+  details?: {
+    createAt: Date;
+    updateAt: Date;
+  }
+}
+
+
+const page1: Base = {
   title: 'The awesome page',
   likes: 100,
   accounts: ['Max', 'Anton', 'Nikita'],
@@ -13,11 +25,11 @@ const page1 = {
   }
 }
 
-const page2 = {
+const page2: Base = {
   title: 'Python or Js',
   likes: 5,
   accounts: ['Alex'],
   status: 'close',
 }
 
-export {};
+export { };
